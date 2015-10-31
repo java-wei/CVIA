2b5e7b27b7de86e8c78014a69d3306e1c917f84e
aieryiran <aieryiran@gmail.com>
2015-10-31 14:28:29 +0800

    Fix the bug in pdfExtractor.php

--- pdfExtractor.php
+++ pdfExtractor.php
@@ -5,11 +5,11 @@ function extractPDF($file_path) {
 	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
 		return shell_exec("D:\cygwin\bin\bash.exe --login  -c 'pdf2txt.py $file_path'");
 	} else {
-    	return shell_exec('pdf2txt.py $file_path');
+    	return shell_exec("pdfminer/build/scripts-2.7/pdf2txt.py $file_path");
 	}
 }
 
 
-// echo extractPDF('/cygdrive/d/xampp/htdocs/CVIA/CVs/LinkedIn/RussellOng.pdf');
+echo extractPDF('/Users/wenyiran/Documents/WenYiran_Resume.pdf');
 
 ?>
