4de58660eb1395a8104e3c696b119def6e9ff4e5
Yingjie Cheng <chengyingjie29@gmail.com>
2015-11-07 21:38:27 +0800

    Add file to support adding job

---
+++ postJob.php
@@ -0,0 +1,25 @@
+<?php
+require_once('includes/config.php');
+
+$keyword = $_COOKIE['keyword'];
+$importance = $_COOKIE['importance'];
+$user_id = $_SESSION['id'];
+
+//process login form if submitted
+if(isset($_POST['submit'])){
+	$jobName = $_POST['jobname'];
+	$jobCompany = $_POST['jobCompany'];
+	$jobDescription = $_POST['jobDescription'];
+
+	$sql = "INSERT INTO ".JOB_TABLE." (owner_id, job_title, job_description, job_keyword, keyword_importance, job_company)
+                VALUES ('$user_id', '$jobName', '$jobDescription', '$keyword', '$importance', '$jobCompany');";
+    $result = mysql_query($sql);
+    // echo mysql_errno($connector) . ": " . mysql_error($connector). "\n";
+}
+
+header('Location: jobPortal.php');	
+
+
+//include header template
+require('layout/header.php'); 
+?>
